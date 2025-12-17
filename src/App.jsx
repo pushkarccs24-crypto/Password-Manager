e151c806b748137537b5df162a6641d2d006a4ee
pushkarccs24-crypto <pushkarc.cs24@bmsce.ac.in>
2025-12-17 05:52:11 +0000

    Added login route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Routes>
          <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} /> 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/add-password" element={<AddPassword />} />
